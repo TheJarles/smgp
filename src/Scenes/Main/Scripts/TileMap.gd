ea31f44b68e4b6ed9ff67bd36536e8bae8c60bbb
Johannes Skogman <johannes.skogman@gmail.com>
2019-11-08 18:30:46 +0100

    make player unable to buffer jump on spikes

--- src/Scenes/Main/Scripts/TileMap.gd
+++ src/Scenes/Main/Scripts/TileMap.gd
@@ -10,11 +10,16 @@ func _on_actor_collided(collision, actor):
 		var tile_position = world_to_map(actor.position)
 		tile_position -= collision.normal * 2
 		var tile = get_cellv(tile_position)
-		if tile == 3 and actor.get_node("PlayerStateMachine").current_state != actor.get_node("PlayerStateMachine").get_node("Staggering"):
-			print(actor.get_node("PlayerStateMachine").state_names)
-			print("Current State Node: ", actor.get_node("PlayerStateMachine").current_state)
-			print("Current State: ", actor.get_node("PlayerStateMachine").current_state_to_string())
-			print("Entering stagger state")
-			actor.get_node("PlayerStateMachine").current_state.damage = true
-			actor.get_node("PlayerStateMachine").current_state.emit_signal("finished", "staggering")
-			print("Current State: ", actor.get_node("PlayerStateMachine").current_state_to_string())
+		var player_state_machine = actor.get_node("PlayerStateMachine")
+		var current_state = player_state_machine.current_state
+		if tile == 3 and current_state != player_state_machine.get_node("Staggering"):
+#			print(actor.get_node("PlayerStateMachine").state_names)
+#			print("Current State Node: ", actor.get_node("PlayerStateMachine").current_state)
+#			print("Current State: ", actor.get_node("PlayerStateMachine").current_state_to_string())
+#			print("Entering stagger state")
+			actor.get_node("BufferTimer").stop()
+			current_state.damage = true
+			if current_state.get_name() in ["Jumping", "Falling"]:
+				current_state._on_timed_out()
+			current_state.emit_signal("finished", "staggering")
+#			print("Current State: ", actor.get_node("PlayerStateMachine").current_state_to_string())
