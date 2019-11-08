ff2aa97b9644cd4ce6c1779970dff2fef1137960
Johannes Skogman <johannes.skogman@gmail.com>
2019-11-08 02:46:43 +0100

    implement stagger trigger on collision with spikes

--- src/Scenes/Main/Scripts/TileMap.gd
+++ src/Scenes/Main/Scripts/TileMap.gd
@@ -1,5 +1,6 @@
 extends TileMap
 
+
 func _ready():
 	owner.get_node("Player").connect("collided", self, "_on_actor_collided")
 
@@ -8,3 +9,9 @@ func _on_actor_collided(collision, actor):
 	if collision.collider == self:
 		var tile_position = world_to_map(actor.position)
 		print(tile_position)
+		print(collision.normal)
+		tile_position -= collision.normal * 2
+		var tile = get_cellv(tile_position)
+		if tile == 3 and actor.get_node("StateMachine").current_state != actor.get_node("Staggering"):
+			actor.get_node("StateMachine")._change_state("staggering")
+			print(actor.get_node("StateMachine").state_names[actor.get_node("StateMachine").current_state])
